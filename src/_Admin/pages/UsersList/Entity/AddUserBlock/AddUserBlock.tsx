dd365990a6290c343b54040f23d88a8268773ec8
OnlyBlack <aera.onlyblack@gmail.com>
2024-12-06 17:50:07 +0500

    feat: button to add a user admin-side
    - added styles
    - timer for security
    - invalidation of UsersList table and display of new data in it

---
+++ src/_Admin/pages/UsersList/Entity/AddUserBlock/AddUserBlock.tsx
@@ -0,0 +1,66 @@
+import "./AddUserBlock.css";
+import { useEffect, useState } from "react";
+import { Button } from "@mui/material";
+import { useDispatch } from "react-redux";
+import { useAppSelector } from "@store/store";
+import { getEmployeeId } from "@Admin/auth/_auth.slice";
+import { AlertMessage } from "@ui/AlertMessage/AlertMessage";
+import { usersListApi } from "@Admin/pages/UsersList/_usersListApi";
+
+export const AddUserBlock = () => {
+  const dispatch = useDispatch();
+  const employeeId = useAppSelector(getEmployeeId);
+
+  const [isOpenAlertSuccess, setOpenAlertSuccess] = useState<boolean>(false);
+  const [isOpenAlertError, setOpenAlertError] = useState<boolean>(false);
+  const [isDisableAddButton, setDisableAddButton] = useState<boolean>(false);
+
+  const invaldateUsersTable = () =>
+    dispatch(usersListApi.util.invalidateTags(["UserList"]));
+  const [createUser] = usersListApi.useCreateUserMutation();
+
+  const handlClickAddUser = async () => {
+    if (!employeeId) return;
+    await createUser(employeeId).then((res) => {
+      if (res?.data) {
+        invaldateUsersTable();
+        setDisableAddButton(true);
+        setOpenAlertSuccess(true);
+      }
+      if (res?.error) return setOpenAlertError(true);
+    });
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDisableAddButton(false);
+    }, 5000);
+    return () => clearTimeout(timer);
+  }, [isDisableAddButton]);
+
+  return (
+    <>
+      <div className="users-list__add-user">
+        <Button
+          variant="contained"
+          onClick={handlClickAddUser}
+          disabled={isDisableAddButton}
+        >
+          Добавить пользователя
+        </Button>
+      </div>
+      <AlertMessage
+        isOpen={isOpenAlertSuccess}
+        onClose={() => setOpenAlertSuccess(false)}
+        type="success"
+        message="Пользователь успешно добавлен"
+      />
+      <AlertMessage
+        isOpen={isOpenAlertError}
+        onClose={() => setOpenAlertError(false)}
+        type="error"
+        message="Ошибка при создании пользователя"
+      />
+    </>
+  );
+};
--- src/_Admin/pages/UsersList/_usersListApi.ts
+++ src/_Admin/pages/UsersList/_usersListApi.ts
@@ -1,13 +1,26 @@
 import { adminApi } from "@Admin/utils/utils";
 import { User } from "@Admin/utils/types";
+import { AuthResponseData } from "@Admin/auth/types";
 
-const adminApiWithTag = adminApi.enhanceEndpoints({ addTagTypes: ["UserList"] });
+const adminApiWithTag = adminApi.enhanceEndpoints({
+  addTagTypes: ["UserList"],
+});
 
 export const usersListApi = adminApiWithTag.injectEndpoints({
   endpoints: (builder) => ({
     getUsers: builder.query<User[], void>({
       query: () => "users",
       providesTags: ["UserList"],
+    }),
+    createUser: builder.mutation<
+      { message: string },
+      AuthResponseData["employeeId"]
+    >({
+      query: (employeeId) => ({
+        url: "users",
+        method: "POST",
+        body: { employeeId },
+      }),
     }),
   }),
 });
